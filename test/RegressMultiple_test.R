eae72160a83d12d14dedf109bdc24dcf2fed7691
ransonr <ryan.ranson@gmail.com>
2014-07-20 09:44:13 -0700

    Updated RegressMultiple_test.R to use returns

--- test/RegressMultiple_test.R
+++ test/RegressMultiple_test.R
@@ -3,11 +3,11 @@ require(quantmod)
 source("GitHub/quantfin/R/RegressMultiple.R")
 
 # Lazy way to get the data we need - model doesn't mean anything
-mdl <- specifyModel(Ad(SPY) ~ Ad(IWM) + Ad(HYG) + Ad(TLT) + Ad(VXX))
+mdl <- specifyModel(Delt(Ad(SPY)) ~ Delt(Ad(IWM)) + Delt(Ad(HYG)) + Delt(Ad(TLT)) + Delt(Ad(VXX)))
 mdl.data <- modelData(mdl)
 
-outputs <- data.frame(mdl.data$Ad.SPY, mdl.data$Ad.IWM)
-inputs <- data.frame(mdl.data$Ad.HYG, mdl.data$Ad.TLT, mdl.data$Ad.VXX)
+outputs <- data.frame(mdl.data$Delt.Ad.SPY, mdl.data$Delt.Ad.IWM)
+inputs <- data.frame(mdl.data$Delt.Ad.HYG, mdl.data$Delt.Ad.TLT, mdl.data$Delt.Ad.VXX)
 
 res <- RegressMultiple(outputs, inputs)
 
